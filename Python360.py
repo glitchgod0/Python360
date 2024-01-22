ecfda160219b35da337904fcd23a93432dc3d0f8
Glitchgod <glitchgod010110@gmail.com>
2024-01-22 01:34:16 +0000

    Update Python360.py

--- Python360.py
+++ Python360.py
@@ -3,14 +3,37 @@ import os
 
 print("Python360 Test by Glitchgod")
 
+DEBUG = True
+
 if len(sys.argv) != 1: #Check if theres arguments
 	FilePath = sys.argv[1] #set FilePath to Argument
 	if os.path.exists(FilePath): #Check if FilePath is actually a valid path
-		print(f"File Path = {FilePath}") #print FilePath
-		file = open(FilePath, "rb+") #Open File
-		filetype = file.read(4) #Read the first 4 bytes
-		filetype=str(filetype, 'utf-8') #removes the b from the start of the string
-		print(f"File Type: {filetype}") #print file type
-		file.close()	
+		if DEBUG == True:
+			print(f"[DEBUG] File Path = {FilePath}") #print FilePath
+
+		File = open(FilePath, "rb+") #Open File
+		FileType = File.read(4) #Read the first 4 bytes
+		FileType=str(FileType, 'utf-8') #removes the b from the start of the string
+
+		if DEBUG == True:
+			print(f"[DEBUG] File Type: {FileType}") #print file type
+
+			if FileType == "CON ":
+				print("CON File")
+				CON = True
+			elif FileType == "LIVE":
+				print("LIVE File")
+				LIVE = True
+			elif FileType == "XEX2":
+				print("XEX File")
+				XEX2 = True
+			else:
+				print("Not Recognized")
+
+		if CON == True:
+			File.seek(31)
+			File.read(1)
+
+		File.close()	
 else:
 	print("No file path argument given.")
