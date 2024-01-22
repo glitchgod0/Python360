70bc21fe2dfad0a9f18ccb6a3420bd4d3bbf4afe
Glitchgod <glitchgod010110@gmail.com>
2024-01-22 02:11:35 +0000

    Update Python360.py

--- Python360.py
+++ Python360.py
@@ -1,9 +1,40 @@
 import sys
 import os 
 
+
 print("Python360 Test by Glitchgod")
 
 DEBUG = True
+
+
+def ConHandler():
+	File.seek(31)
+	if File.read(1) == b'\x02':
+		print("02 Retail")
+	elif File.read(1) == b'\x01':
+		print("01 Devkit")
+	else:
+		print("Unknown signing")
+
+def LiveHandler():
+	print("LIVE/PIRS handling not ready!")
+
+def XEXHandler():
+	print("XEX Header")
+	File.seek(4)
+	print(File.read(4))
+	File.seek(8)
+	print(File.read(4))
+	File.seek(12)
+	print(File.read(4))
+	File.seek(16)
+	print(File.read(4))
+	File.seek(20)
+	print(File.read(4))
+	File.seek(24)
+	print(File.read(4))
+
+
 
 if len(sys.argv) != 1: #Check if theres arguments
 	FilePath = sys.argv[1] #set FilePath to Argument
@@ -20,19 +51,19 @@ if len(sys.argv) != 1: #Check if theres arguments
 
 			if FileType == "CON ":
 				print("CON File")
-				CON = True
+				ConHandler()
 			elif FileType == "LIVE":
 				print("LIVE File")
 				LIVE = True
+				LiveHandler()
 			elif FileType == "XEX2":
 				print("XEX File")
 				XEX2 = True
+				XEXHandler()
 			else:
 				print("Not Recognized")
 
-		if CON == True:
-			File.seek(31)
-			File.read(1)
+
 
 		File.close()	
 else:
